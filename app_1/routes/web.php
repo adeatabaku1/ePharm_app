3e1df437009dd2cf0946aa3e43125ef4853994dc
Adea <tabakuadea3@gmail.com>
2025-03-15 22:39:15 +0100

    Using Eloquent and creating Migrations

--- app_1/routes/web.php
+++ app_1/routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Models\Tenant;
+
+//veq testim a shfaqet json ne POSTMAN edhe me kqyr a futet ne databaze
+Route::get('/test-insert', function () {
+    $pharmacy = Pharmacy::create([
+        'name' => 'SunMed Pharmacy',
+        'email' => 'sunmedpharmacy@gmail.com',
+        'phone' => '555-123-4567',
+        'address' => '789 Oak Avenue'
+    ]);
+
+    return response()->json($pharmacy);
 });
