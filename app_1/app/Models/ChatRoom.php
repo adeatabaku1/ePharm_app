f42ebfdb1e4b149f27214753fe1117169b9e340e
Fatjeta <fatjeta.gashi3@student.uni-pr.edu>
2025-03-31 02:28:59 +0200

    Add ChatRoom.php

--- app_1/app/Models/ChatRoom.php
+++ app_1/app/Models/ChatRoom.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChatRoom extends Model
 {
-    //
+    protected $fillable = [
+        'doctor_id',
+        'patient_id',
+        'created_at',
+    ];
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
 }
