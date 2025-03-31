dec580402067275d59080fee5aa39f6dd191f8cf
Fatjeta <fatjeta.gashi3@student.uni-pr.edu>
2025-03-31 02:25:24 +0200

    Add SupplierOrderItem.php

--- app_1/app/Models/SupplierOrderItem.php
+++ app_1/app/Models/SupplierOrderItem.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class SupplierOrderItem extends Model
 {
-    //
+    protected $fillable = [
+        'supplier_order_id',
+        'medication_name',
+        'quantity',
+        'unit_price',
+        'subtotal',
+    ];
+
+    public function supplierOrder()
+    {
+        return $this->belongsTo(SupplierOrder::class);
+    }
 }
