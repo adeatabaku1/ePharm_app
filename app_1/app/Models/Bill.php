fbaa6b9c9ddf80c2b0acbe9faf7d81ba90234acc
Fatjeta <fatjeta.gashi3@student.uni-pr.edu>
2025-03-31 02:26:03 +0200

    Add Bill.php

--- app_1/app/Models/Bill.php
+++ app_1/app/Models/Bill.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bill extends Model
 {
-    //
+    protected $fillable = [
+        'pharmacy_id',
+        'patient_id',
+        'sale_id',
+        'payment_method',
+        'total_price',
+        'created_at',
+    ];
+
+    public function pharmacy()
+    {
+        return $this->belongsTo(Pharmacy::class);
+    }
+
+    public function patient()
+    {
+        return $this->belongsTo(Patient::class);
+    }
 }
