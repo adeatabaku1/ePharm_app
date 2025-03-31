9049297dec90f0081d0624197645a438754b9e4e
Fatjeta <fatjeta.gashi3@student.uni-pr.edu>
2025-03-31 02:30:23 +0200

    Add MedicineLog.php

--- app_1/app/Models/MedicineLog.php
+++ app_1/app/Models/MedicineLog.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class MedicineLog extends Model
 {
-    //
+    protected $fillable = [
+        'medicine_id',
+        'user_id',
+        'action',
+        'description',
+        'quantity_change',
+        'created_at',
+    ];
 }
