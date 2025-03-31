aa1484662c10cc8a74491344628cf7cc3a697bd6
Fatjeta <fatjeta.gashi3@student.uni-pr.edu>
2025-03-31 02:27:53 +0200

    Add MedicationIntakeReminder.php

--- app_1/app/Models/MedicationIntakeReminder.php
+++ app_1/app/Models/MedicationIntakeReminder.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class MedicationIntakeReminder extends Model
 {
-    //
+    protected $fillable = [
+        'patient_id',
+        'prescription_item_id',
+        'scheduled_time',
+        'sent_at',
+        'created_at',
+    ];
 }
+
