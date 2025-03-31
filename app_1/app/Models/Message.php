2dcfee606a3f6c7e27912a7066a5d8748e8ddb61
Fatjeta <fatjeta.gashi3@student.uni-pr.edu>
2025-03-31 02:29:48 +0200

    Add Message.php

--- app_1/app/Models/Message.php
+++ app_1/app/Models/Message.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    //
+    protected $fillable = [
+        'chat_room_id',
+        'sender_id',
+        'message',
+        'sent_at',
+    ];
+
+    public function chatRoom()
+    {
+        return $this->belongsTo(ChatRoom::class);
+    }
 }
