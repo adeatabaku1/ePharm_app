3596129b259c6390edb774cc88d1693b43e08fcd
ArtinaQorrolli <artina.qorrolli@student.uni-pr.edu>
2025-05-14 13:38:04 +0200

    Revert "Add Doctor module: controllers, services, requests, and resources"
    This reverts commit d01ad4c472c78efca8f2e7cdc7873b4fec5d8aa7.

--- app_1/app/Http/Requests/Doctor/StoreNotificationRequest.php
+++ app_1/app/Http/Requests/Doctor/StoreNotificationRequest.php
@@ -8,7 +8,7 @@ class StoreNotificationRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return true; 
+        return true; // Mund të shtosh logjikë për autorizim nëse nevojitet
     }
 
     public function rules(): array
